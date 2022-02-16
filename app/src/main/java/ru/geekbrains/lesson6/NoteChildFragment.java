a2b76e08936a92a1ba7203e59ed4b34ef0b7b850
Sergey <emelyanchik1980@gmail.com>
2022-02-16 20:43:55 +0300

    Урок №7 добавил ChildFragment

--- app/src/main/java/ru/geekbrains/lesson6/NoteChildFragment.java
+++ app/src/main/java/ru/geekbrains/lesson6/NoteChildFragment.java
@@ -12,14 +12,14 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 
-public class NoteFragment extends Fragment {
+public class NoteChildFragment extends Fragment {
 
     public static final String ARG_MENU = "menu";
 
     private Menu menu;
 
-    public static NoteFragment newInstance(Menu menu) {
-        NoteFragment fragment = new NoteFragment();
+    public static NoteChildFragment newInstance(Menu menu) {
+        NoteChildFragment fragment = new NoteChildFragment();
         Bundle bundle = new Bundle();
         bundle.putParcelable(ARG_MENU, menu);
         fragment.setArguments(bundle);
@@ -30,29 +30,24 @@ public class NoteFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_note, container, false);
+        return inflater.inflate(R.layout.fragment_note_child, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         menu = getArguments().getParcelable(ARG_MENU);
-
-
-        getChildFragmentManager().beginTransaction().replace(R.id.container_clild, NoteChildFragment.newInstance(menu)).addToBackStack("").commit();
+        TextView textView = view.findViewById(R.id.notes);
+        TypedArray note = getResources().obtainTypedArray(R.array.note);
+        textView.setText(note.getResourceId(menu.getIndex(), R.id.note));
 
 
         view.findViewById(R.id.but_back).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                requireActivity().getSupportFragmentManager().popBackStack();
-            }
-        });
-        view.findViewById(R.id.but_back2).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
                 getParentFragmentManager().popBackStack();
             }
         });
+
     }
 }
